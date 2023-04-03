f75a36ac861c5305cc1d8f4262b7dbd8a91fa2d2
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-03 19:41:58 -0400

    add separate layout for blog

--- src/app/(Blog)/layout.tsx
+++ src/app/(Blog)/layout.tsx
@@ -1,19 +1,16 @@
-import Header from "@components/Header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Noelle L'Amour",
-  description: "Portfolio",
+  title: "AI Blogging",
+  description: "An AI Blogging about AI things.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Header />
         <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 mt-[4rem]">{children}</div>
       </body>
     </html>
   );
 }
-0
