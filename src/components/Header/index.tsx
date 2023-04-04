f0ff6e4d38c6e9aa9ad5e5a25589fe0c31c996b1
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-04 09:05:35 -0400

    refactor Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
 
+const menu = [
+  ["/", "Home"],
+  ["/about", "About"],
+  ["/projects", "Projects"],
+  ["/contact", "Contact"],
+];
+
+const navigationMenu = menu.map(([url, text]) => {
+  return (
+    <Link href={url} key="url">
+      <span className="text-[1.4rem]">{text}</span>
+    </Link>
+  );
+});
+
 const Header = () => {
   return (
-    <header className={styles["header-nav"]}>
+    <header className="fixed grid content-center w-[100%] h-[3rem] top-0 left-0 bg-{--bg-primary-hsl} shadow-[0_0_0.5rem_0.2rem_hsla(0,0%,0%,0.4)]">
       <nav className="h-[100%] w-[100%] flex justify-evenly md:w-[80%] place-self-center">
-        <Link href="/" className={styles["nav-item"]}>
-          <span className={styles["nav-text"]}>Home</span>
-        </Link>
-        <Link href="/about" className={styles["nav-item"]}>
-          <span className={styles["nav-text"]}>About</span>
-        </Link>
-        <Link href="/projects" className={styles["nav-item"]}>
-          <span className={styles["nav-text"]}>Projects</span>
-        </Link>
-        <Link href="/contact" className={styles["nav-item"]}>
-          <span className={styles["nav-text"]}>Contact</span>
-        </Link>
+        {navigationMenu}
       </nav>
     </header>
   );
