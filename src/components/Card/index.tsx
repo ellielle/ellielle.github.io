4f9e494e85411698b97d9b8bcf1e8de56d50dc8f
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-04 09:19:25 -0400

    refactor Card component to tailwind

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,15 @@
-import styles from "./Card.module.css";
 import { Project } from "@helpers/skillset";
+
+// TODO does it need container class with styles.container?
+const cardStyle = ["grid", "p-[1rem]", "justify-self-center", "container"];
 
 const Card = ({ children, project }: { children: React.ReactNode; project: Partial<Project> }) => {
   return (
-    <div className={styles.container}>
+    <div className="grid p-[1rem] justify-self-center border-[solid_1px_hsl(231,15%,18%)] rounded-lg shadow-[0_0_0.5rem_hsl(34,51%,43%)]">
       <div className="text-green-400">{project.name}</div>
-      <div className="">{children}</div>
-      <div className={styles.description}>{project.description}</div>
-      <div className={styles.stack}>{project.stack}</div>
+      <div className="py-4">{children}</div>
+      <div>{project.description}</div>
+      <div>{project.stack}</div>
     </div>
   );
 };
