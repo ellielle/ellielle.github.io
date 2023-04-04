5a32a0f8f31e942cece9a2f5c761af6ff8adeab9
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-04 15:03:06 -0400

    work on card style and responsiveness

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import { Project } from "@helpers/skillset";
 
 const Card = ({ children, project }: { children: React.ReactNode; project: Partial<Project> }) => {
   return (
-    <div className="grid text-center p-[1rem] border-[solid_1px_hsl(231,15%,18%)] rounded-lg shadow-[0_0_0.5rem_hsl(34,51%,43%)]">
+    <div className="card-container">
       <div className="text-green-400">{project.name}</div>
       <div className="py-4 grid">{children}</div>
       <div>{project.description}</div>
