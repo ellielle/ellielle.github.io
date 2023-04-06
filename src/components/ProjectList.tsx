099991fdbd0e265a2dd20d0383d4061457e0a31a
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-06 09:35:47 -0400

    move project list to a component

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Card from "@components/Card";
+import { projectList, Project } from "@utils/skillset";
+
+const ProjectList: React.FC<{ filterSelected: boolean }> = ({ filterSelected = false }) => {
+  let list: JSX.Element[] = [];
+  let mappableList: Project[] = [];
+
+  if (filterSelected) {
+    mappableList = projectList.filter((project) => project.select);
+  } else {
+    mappableList = projectList;
+  }
+
+  list = mappableList.map((project) => {
+    return (
+      <Card key={project.name} project={project}>
+        <Image
+          src={project.image}
+          alt={project.imageAlt}
+          width={1794}
+          height={946}
+          className="justify-self-center w-[50%] h-auto md:w-[20%]"
+          priority
+        />
+      </Card>
+    );
+  });
+
+  return <>{list}</>;
+};
+
+export default ProjectList;
