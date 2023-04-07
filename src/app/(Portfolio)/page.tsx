87b5f4a082c452a969209d0346ace0ce3001b297
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-07 13:54:15 -0400

    make adjustments to logos and grid

--- src/app/(Portfolio)/page.tsx
+++ src/app/(Portfolio)/page.tsx
@@ -6,24 +6,24 @@ const Home: React.FC = () => {
   return (
     <main className={inter.className}>
       <div className="text-left">
-        <h1 className="text-3xl gradient-text">Hey, I'm Noelle L&#39;Amour 👋</h1>
+        <h1 className="text-3xl">Hey, I'm <span className="gradient-text">Noelle L&#39;Amour</span> 👋</h1>
         <div className="mb-10">
           {/* TODO finish intro */}
           {/* have very dim fading css around kalamazoo saying "it exists ?" */}
           <p className="text-xl">
-            I'm a full stack developer from the mythical city of Kalamazoo, where I've learned to
-            weave the <span className="">infernal</span> magicks of "Web Dev".
+            I'm a full stack web developer from the mythical city of Kalamazoo, where I've learned
+            to weave the <span className="text-red-600">infernal</span> magicks of <span className="text-yellow-400">"Web Dev"</span>.
           </p>
         </div>
 
         {/* TODO on the chopping block */}
-        <Link href="/about" className="text-xl">
+        {/* <Link href="/about" className="text-xl">
           Read My Tale --&gt;
-        </Link>
+        </Link> */}
         {/* TODO  */}
 
-        <h2 className="mt-10 text-center text-xl">Some of my favorite schools of magick</h2>
-        <section className="mt-8 grid grid-cols-2 grid-flow-row gap-5 place-content-center justify-items-center md:grid-cols-3 w-[60%] mx-auto gap-y-20 lg:grid-cols-[repeat(auto-fill,minmax(20%,100px))]">
+        <h2 className="mt-20 text-center text-xl">Some of my favorite schools of magick</h2>
+        <section className="mt-[5.5rem] grid grid-cols-2 grid-flow-row gap-5 place-content-center justify-items-center md:grid-cols-3 w-[60%] mx-auto gap-10 lg:grid-cols-[repeat(auto-fill,minmax(20%,100px))]">
           {/* CSS glow effect, maybe use a spell circle svg and make it transparent */}
           <Vue />
           <Nuxt />
