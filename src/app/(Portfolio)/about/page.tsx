e220b93931c1dc1e450cb43898c0ef10a10ff254
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-13 12:23:45 -0400

    do some creative writing

--- src/app/(Portfolio)/about/page.tsx
+++ src/app/(Portfolio)/about/page.tsx
@@ -1,39 +1,66 @@
+import { Fragment } from "react";
+import { usePathname } from "next/navigation";
+
+const ShortBlurb: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="text-center">
+      <h1 className="mt-[6rem] text-2xl">A bit about myself</h1>
+
+      <p className="text-lime-300 mt-8 text-xl">
+        I&#39;ve been playing with programming as a hobby on and off for about the last 20 years. I
+        started with making simple websites with HTML and CSS on Geocities. I was around 13 or so
+        when I picked up <a href="https://www.graalonline.com/">Graal Online</a>, which eventually
+        introduced player-made worlds. I talked my way into a group and started to learn very simple
+        C++. Some of my code made it into the main game server (at the time), so it's quite possible
+        it's still there! Though I hope for their sake it isn't. 😅
+      </p>
+      <p
+        className={[
+          typeof children === "string" ? "image-mask" : "",
+          "text-orange-300 mt-8 text-xl",
+        ].join(" ")}>
+        I lost interest after my second year of university, but still played around with some small
+        projects in C# and Ruby over the years. I also made a couple small games with GameMaker, but
+        they've been lost to time.
+      </p>
+      {children}
+    </div>
+  );
+};
+
+const RestOfTheOwl: React.FC = () => {
+  return (
+    <Fragment>
+      <p className="text-orange-300 mt-8 text-xl">
+        Around the middle of 2018, I decided to finally give in to the Call of the :nth Child and
+        started my journey toward more than hobby coding. Originally, I picked up Ruby and Rails and
+        followed a few courses. I wasn&#39;t really feeling Ruby by the end of the course, but the
+        experience of properly structured learning and a deeper understanding of concepts in general
+        brought back the spark.
+      </p>
+      <p className="text-orange-300 mt-8 text-xl">
+        I picked up JavaScript afterward and decided to focus more on web development. JavaScript
+        and I vibe together pretty well, I've been expanding my knowledge and toolkit since. I'm
+        currently working on a deeper understanding of the frameworks I use, and the language
+        itself. I've also picked up Go recently, though I haven't had much time to play with it yet.
+      </p>
+      <p className="text-orange-300 mt-8 text-xl">
+        Once I felt confident enough in doing so, I emerged back out into the world and began
+        freelancing. I've worked on a projects from small in scope to a full serverless web app that
+        consumes multiple APIs. I also donate time to a not-for-profit from my home town when they
+        need updates to their WordPress site.
+      </p>
+    </Fragment>
+  );
+};
+
 const About: React.FC = () => {
   return (
-    <main>
-      <div className="text-left">
-        <div className="text-red-500">Skip Sob Story</div>
-
-        {/* TODO needs more other info not programming related, less sob story */}
-        <h1 className="mt-8 text-2xl">As the landing said, I&#39;m Noelle L&#39;Amour 👋</h1>
-        <p className="text-lime-300 mt-8 text-xl">
-          I&#39;ve been playing with coding as a hobby on and off for about the last 20 years. This
-          never amounted to anything other than small side projects or code contributions. I never
-          really got my interest to stick like it had in my early years of coding.
-        </p>
-        <p className="text-orange-300 mt-8 text-xl">
-          Around 2016, my body just gave up on everything all at once.
-        </p>
-        <p className="text-orange-300 mt-8 text-xl">
-          Around the middle of 2018, I decided to finally give in to the Call of the :nth Child and
-          started my journey toward being a developer. Originally, I picked up Ruby and Rails and
-          followed a few courses. I wasn&#39;t really feeling Ruby by the end of the course, but it was
-          helpful in learning other concepts that carry over to any language.
-        </p>
-        <p className="text-orange-300 mt-8 text-xl">
-          I picked up JavaScript after that, since most resources I found focused on JS-related
-          stacks and how amazing its impossible number of libraries are! JavaScript and I vibed
-          together pretty well, so I stuck with it.
-        </p>
-        <p className="text-orange-300 mt-8 text-xl">
-          Once I felt confident enough in doing so, I stepped back out into the world and started
-          freelancing.
-        </p>
-        {/* TODO maybe a lot less sob story, mention the jamstack, serverless, projects */}
-        {/* TODO and mention work with BCHS */}
-      </div>
+    <main className="px-4">
+      <ShortBlurb>{<RestOfTheOwl />}</ShortBlurb>
     </main>
   );
 };
 
+export { ShortBlurb };
 export default About;
