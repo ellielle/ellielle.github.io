1b172ddf58254ecb581e40d19e23099ed944af13
ellielle <40385743+ellielle@users.noreply.github.com>
2023-04-19 10:39:53 -0400

    add class to svg files

--- src/components/Logos/index.tsx
+++ src/components/Logos/index.tsx
@@ -5,19 +5,16 @@ import { React } from "./React";
 import { Netlify } from "./Netlify";
 import { Vercel } from "./Vercel";
 import { TypeScript } from "./TypeScript";
-import { Discord } from "./Social/Discord";
+// import { Discord } from "./Social/Discord";
 import { Email } from "./Social/Email";
 import { LinkedIn } from "./Social/LinkedIn";
-import { Twitter } from "./Social/Twitter";
-import { Github } from "./Social/Github";
+// import { Twitter } from "./Social/Twitter";
+import { GitHub } from "./Social/GitHub";
 
-export const techStacks = [
-  <TypeScript key="1" />,
-  <Vue key="2" />,
-  <Nuxt key="3" />,
-  <Netlify key="4" />,
-  <React key="5" />,
-  <Next key="6" />,
-  <Vercel key="7" />,
-];
-export const socialMedia = { Discord, Email, LinkedIn, Twitter, Github };
+export const techStacks = [TypeScript, Vue, Nuxt, Netlify, React, Next, Vercel].map(
+  (Component, index) => <Component key={`${Component.name}-${index}`} />
+);
+
+export const socialMedia = [Email, LinkedIn, GitHub].map((Component, index) => (
+  <Component key={`${Component.name}-${index}`} />
+));
