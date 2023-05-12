b1ffc949dc5f70ec5c5afb0617a901208091d893
ellielle <40385743+ellielle@users.noreply.github.com>
2023-05-12 10:45:46 -0400

    split metadata to make it more easily reusable

--- src/utils/shared-metadata.ts
+++ src/utils/shared-metadata.ts
@@ -3,34 +3,11 @@ import type { Metadata } from "next";
 // TODO replace lazy metadata
 // TODO either add the built-in support for a sitemap or double-check the plugin's sitemap
 // TODO maybe split metadata into importable chunks
-export const sharedMetadata: Metadata = {
-  metadataBase: new URL("https://noellelamour.vercel.app/"),
-  title: {
-    template: "%s | Noelle L'Amour",
-    default: "Noelle L'Amour",
-  },
-  description: "Full-stack Developer",
-  other: {
-    custom: "meta",
-    "google-site-verification": "pT0tr8qa4dd8GQR-SwLSyuag8A_dzwo2UJE0TmXDjCs",
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: dark)", color: "#1b2836" },
-    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
-  ],
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-    },
-  },
+export const openGraphMetadata: Metadata = {
   openGraph: {
-    title: "Noelle L'Amour",
-    description: "Portfolio",
-    siteName: "Noelle L'Amour",
+    title: "Noelle L'Amour's Fantastical Portfolio",
+    description: "Your Next Developer",
+    siteName: "A Neat Portfolio",
     type: "website",
     locale: "en_US",
     url: "https://noellelamour.vercel.app/",
@@ -44,11 +21,47 @@ export const sharedMetadata: Metadata = {
       },
     ],
   },
+};
+
+export const twitterMetadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Noelle L'Amour",
     // TODO creator: "",
   },
+};
+
+export const customMetadata: Metadata = {
+  other: {
+    custom: "meta",
+    "google-site-verification": "pT0tr8qa4dd8GQR-SwLSyuag8A_dzwo2UJE0TmXDjCs",
+  },
+};
+
+export const robotsMetadata: Metadata = {
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+    },
+  },
+};
+
+export const sharedMetadata: Metadata = {
+  metadataBase: new URL("https://noellelamour.vercel.app/"),
+  title: {
+    template: "%s | Noelle L'Amour",
+    default: "Noelle L'Amour",
+  },
+  description: "Full-stack Developer",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#1b2836" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
+
   icons: {
     // TODO change favicon from default vercel ico
     shortcut: "/favicon.ico",
