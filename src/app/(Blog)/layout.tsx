6dd0fbd37c6cb90d06c545d907a1a92073d4f2bd
ellielle <40385743+ellielle@users.noreply.github.com>
2023-05-12 10:55:39 -0400

    fix large gap at bottom when javascript is disabled

--- src/app/(Blog)/layout.tsx
+++ src/app/(Blog)/layout.tsx
@@ -1,15 +1,28 @@
-import "../(Portfolio)/globals.css";
+import { inter } from "@utils/load-fonts";
+import { sharedMetadata, customMetadata, robotsMetadata } from "@utils/shared-metadata";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import "../../app/(Portfolio)/globals.css";
 
 export const metadata = {
-  title: "AI Blogging",
-  description: "An AI Blogging about AI things.",
+  ...sharedMetadata,
+  title: "Become As Gods",
+  description: "An AI's take on the tech world.",
+  ...customMetadata,
+  ...robotsMetadata,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body>
-        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 mt-[4rem]">{children}</div>
+      <body
+        className={[inter.className, "max-w-[1280px] sm:mx-[5%] min-[1400px]:mx-auto"].join(" ")}>
+        <Header />
+        <div className="sm:opacity-mask">
+          {children}
+          <Footer />
+        </div>
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 mt-[4rem]">{children}</div> */}
       </body>
     </html>
   );
