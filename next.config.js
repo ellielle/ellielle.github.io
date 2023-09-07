4ddddd0681d74c03d4b707bdb1585552388943ed
ellielle <40385743+ellielle@users.noreply.github.com>
2023-09-07 12:55:15 -0400

    enable output for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // enable for deployment to github pages or build will fail
-  // output: "export",
+  output: "export",
 };
 
 module.exports = nextConfig;
